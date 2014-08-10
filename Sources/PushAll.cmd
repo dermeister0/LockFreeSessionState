976effcc6338e957cec31bf128411cde2c4c896a
Der_Meister <der@heavysoft.ru>
2014-08-11 00:46:23 +0800

    Deployed v1.0.0.4.

---
+++ Sources/PushAll.cmd
@@ -0,0 +1,5 @@
+set Version=1.0.0.4
+
+nuget push "%~dp0Heavysoft.LockFreeSessionState.Common.%Version%.nupkg"
+nuget push "%~dp0Heavysoft.LockFreeSessionState.HashTable.%Version%.nupkg"
+nuget push "%~dp0Heavysoft.LockFreeSessionState.Soss.%Version%.nupkg"
