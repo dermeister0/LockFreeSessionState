ab052b171633aeaf2c3f60178c00f758da2992ce
Der_Meister <der@heavysoft.ru>
2014-08-10 23:16:37 +0800

    Introduced LockFreeSessionState.Soss project.

---
+++ Sources/Heavysoft.LockFreeSessionState.Soss/SossSessionStateModule.cs
@@ -0,0 +1,27 @@
+﻿using Heavysoft.Web.SessionState;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heavysoft.LockFreeSessionState.Soss
+{
+    public class SossSessionStateModule : LockFreeSessionStateModule
+    {
+        protected override SessionItem AddNewSessionItem(string sessionId, System.Web.SessionState.ISessionStateItemCollection items, System.Web.HttpStaticObjectsCollection staticObjects)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override SessionItem GetSessionItem(string sessionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void RemoveSessionItem(string sessionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
