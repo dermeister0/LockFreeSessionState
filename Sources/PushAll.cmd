83f3bf68af076900eb5fbcdd5208b8d523bb04d2
Der_Meister <der@heavysoft.ru>
2014-08-29 00:09:10 +0800

    Pushed to NuGet.

--- Sources/PushAll.cmd
+++ Sources/PushAll.cmd
@@ -1,4 +1,4 @@
-set Version=1.0.0.4
+set Version=1.0.4702.0
 
 nuget push "%~dp0Heavysoft.LockFreeSessionState.Common.%Version%.nupkg"
 nuget push "%~dp0Heavysoft.LockFreeSessionState.HashTable.%Version%.nupkg"
