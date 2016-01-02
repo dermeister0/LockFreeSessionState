174c7c5269992a43d919f7d1202b9b4b6278546d
Der_Meister <der@heavysoft.ru>
2016-01-03 04:42:20 +0700

    Fix public key token in transforms.

--- Sources/PushAll.cmd
+++ Sources/PushAll.cmd
@@ -1,5 +1,5 @@
-set Version=1.0.4702.0
+set Version=1.1.0.0
 
-nuget push "%~dp0Heavysoft.LockFreeSessionState.Common.%Version%.nupkg"
-nuget push "%~dp0Heavysoft.LockFreeSessionState.HashTable.%Version%.nupkg"
-nuget push "%~dp0Heavysoft.LockFreeSessionState.Soss.%Version%.nupkg"
+%~dp0\Scripts\nuget push "%~dp0Heavysoft.LockFreeSessionState.Common.%Version%.nupkg"
+%~dp0\Scripts\nuget push "%~dp0Heavysoft.LockFreeSessionState.HashTable.%Version%.nupkg"
+%~dp0\Scripts\nuget push "%~dp0Heavysoft.LockFreeSessionState.Soss.%Version%.nupkg"
